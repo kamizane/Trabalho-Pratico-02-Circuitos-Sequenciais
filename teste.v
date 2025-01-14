410235f42c9c3389d65b48f22ecadf418837a14b
kamizane <julioitabira@hotmail.com>
2025-01-13 22:35:11 -0300

    estados de 1 erro ainda nao criados

--- teste.v
+++ teste.v
@@ -17,7 +17,100 @@ module maquina (
     always @(posedge clk) begin
         if (reset)  estado = inicial;
         else        estado = proximo_estado;
+//duvida na linha a cima
     end
 
-    always @()
+    always @(numero or estado) begin
+        case (estado)
+            inicial:
+            begin
+                if (numero == 0101) proximo_estado = cinco;
+                else begin
+                    proximo_estado = inicial;
+                    LED = 1;
+                end
+            end
+            cinco:
+            begin
+                if (LED) begin
+                    if (numero == 1001) proximo_estado = nove;
+                    else                proximo_estado = falha;
+                end
+                else begin
+                    if (numero == 1001) proximo_estado = nove;
+                    else begin
+                        proximo_estado = cinco;
+                        LED = 1;
+                    end
+                end
+            end
+            nove:
+            begin
+                if (LED) begin
+                    if (numero == 0000) proximo_estado = zero;
+                    else                proximo_estado = falha;
+                end
+                else begin
+                    if (numero == 0000) proximo_estado = nove;
+                    else begin
+                        proximo_estado = falha;
+                        LED = 1;
+                    end
+                end
+            end
+            zero:
+            begin
+                if (LED) begin
+                    if (numero == 1001) proximo_estado = nove_final;
+                    else                proximo_estado = falha;
+                end
+                else begin
+                    if (numero == 1001) proximo_estado = nove_final;
+                    else begin
+                        proximo_estado = zero;
+                        LED = 1;
+                    end
+                end
+            end
+            nove_final:
+            begin
+                if (LED) begin
+                    if (numero == 1000) proximo_estado = oito;
+                    else                proximo_estado = falha;
+                end
+                else begin
+                    if (numero == 1000) proximo_estado = oito;
+                    else begin
+                        proximo_estado = nove_final;
+                        LED = 1;
+                    end
+                end
+            end
+            oito:
+            begin
+                if(LED) begin
+                    if (numero == 0001) proximo_estado = um;
+                    else                proximo_estado = falha;
+                end
+                else begin
+                    if (numero == 0001) proximo_estado = um;
+                    else begin
+                        proximo_estado = oito;
+                        LED = 1;
+                    end
+                end
+            end
+    end
+    always @(estado) begin
+        case(estado)
+        inicial: begin
+            A <= 1'b0;
+            B <= 1'b0;
+            C <= 1'b0;
+            D <= 1'b0;
+            E <= 1'b0;
+            F <= 1'b0;
+            G <= 1'b1;
+        end
+    cinco
 endmodule
