e66051d65ddcb0bda3163941ed4af8c4259e0601
kamizane <julioitabira@hotmail.com>
2025-01-16 14:41:16 -0300

    forma de ondas funcionando

--- maquina.v
+++ maquina.v
@@ -1,27 +1,25 @@
 module maquina(
     input clk, reset, insere,
     input [4:1] numero,
-    output reg LED = 0,
+    output reg LED =0,
     output reg A, B, C, D, E, F, G
 );
     parameter inicial = 4'b0000;
-    parameter cinco = 4'b0101;
-    parameter nove = 4'b1001;
-    parameter zero = 4'b0000;
-    parameter nove_final = 4'b1001;
-    parameter oito = 4'b1000;
-    parameter um = 4'b0001;
+    parameter cinco = 4'b0001;
+    parameter nove = 4'b0010;
+    parameter zero = 4'b0011;
+    parameter nove_final= 4'b0100;
+    parameter oito = 4'b0101;
+    parameter um = 4'b0110;
     parameter falha = 4'b1111;
 
-    reg controle = 0;
     reg [3:0] estado;
     reg [3:0] proximo_estado;
     
+
     always @(posedge clk) begin
-        if (reset)  begin
+        if (reset == 1'b0)  begin
             estado <= inicial;
-            LED = 0;
-            controle = 0;
         end
         else begin 
             estado <= proximo_estado;
@@ -29,8 +27,10 @@ module maquina(
     end
 
     always @(posedge clk) begin
-        proximo_estado = estado;
+		if (insere == 1'b0) begin
         case (estado)
+            um:
+                proximo_estado = um;
             falha:
                 proximo_estado = falha;
             inicial:
@@ -55,36 +55,18 @@ module maquina(
                     end
                 end
             nove:
-                if (controle == 1) begin
-                    if (LED) begin
-                        if (numero == 4'b1000) proximo_estado = oito;
+                if (LED) begin
+                    if (numero == 4'b0000) proximo_estado =zero;
                     else                proximo_estado = falha;
-                    end
-                    else begin
-                        if (numero == 4'b1000) proximo_estado = oito;
-                        else begin
-                        proximo_estado = nove;
-                        LED = 1;
-                            end
-                    end
                 end
                 else begin
-                    if (LED) begin
-                        if (numero == 4'b0000) proximo_estado = zero;
-                        else                proximo_estado = falha;
-                        controle = 1;
-                    end
+                    if (numero == 4'b0000) proximo_estado =zero;
                     else begin
-                        
-                        if (numero == 4'b0000) proximo_estado = zero;
-                        else begin
-                            proximo_estado = nove;
-                            LED = 1;
-                        end
-                        controle = 1;
+                        proximo_estado = nove;
+                        LED = 1;
                     end
                 end
-            zero:
+            zero: begin
                 if (LED) begin
                     if (numero == 4'b1001) proximo_estado = nove_final;
                     else                proximo_estado = falha;
@@ -93,6 +75,19 @@ module maquina(
                     if (numero == 4'b1001) proximo_estado = nove_final;
                     else begin
                         proximo_estado = zero;
+                        LED = 1;
+                    end
+                end
+            end
+            nove_final:
+                if (LED) begin
+                    if (numero == 4'b1000) proximo_estado = oito;
+                    else                proximo_estado = falha;
+                end
+                else begin
+                    if (numero == 4'b1000) proximo_estado = oito;
+                    else begin
+                        proximo_estado = nove_final;
                         LED = 1;
                     end
                 end
@@ -111,9 +106,26 @@ module maquina(
             default:
                 proximo_estado = inicial;
         endcase
+		  end
+		  else begin
+		  if (reset == 1'b0) begin
+					proximo_estado = inicial;
+					LED = 0;
+			end
+		  end
     end
-    always @(estado) begin
-        if (estado == um & LED == 1) begin
+    always @(clk) begin
+		  if (reset == 1'b0) begin
+			A <= 1'b0;
+            B <= 1'b0;
+            C <= 1'b0;
+            D <= 1'b0;
+            E <= 1'b0;
+            F <= 1'b0;
+            G <= 1'b1;
+		  
+		  end
+        else if (estado == um & LED == 1) begin
             A <= 1'b0;
             B <= 1'b0;
             C <= 1'b1;
@@ -122,15 +134,7 @@ module maquina(
             F <= 1'b0;
             G <= 1'b0;
         end
-        // else if (estado == inicial) begin
-        //     A <= 1'b1;
-        //     B <= 1'b1;
-        //     C <= 1'b1;
-        //     D <= 1'b1;
-        //     E <= 1'b1;
-        //     F <= 1'b1;
-        //     G <= 1'b1;
-        // end
+
         else if (estado == um & LED == 0) begin
             A <= 1'b0;
             B <= 1'b1;
@@ -149,7 +153,7 @@ module maquina(
             F <= 1'b0;
             G <= 1'b0;
         end
-        else begin
+        else if (insere == 1'b0) begin
             A <= ~(((~numero[4])&numero[2])|((~numero[4])&(~numero[3])&(~numero[1]))|((~numero[4])&numero[3]&numero[1])|(numero[4]&(~numero[3])&(~numero[2])));
             B <= ~(((~numero[4])&(~numero[3]))|((~numero[3]&(~numero[2])))|((~numero[4])&(~numero[2])&(~numero[1]))|((~numero[4])&numero[2]&numero[1]));
             C <= ~(((~numero[3])&(~numero[2]))|((~numero[4])&numero[1])|((~numero[4])&numero[3]));
--- maquina_tb.v
+++ maquina_tb.v
@@ -17,27 +17,147 @@ module maquina_tb ();
         $display("numero A B C D E F G LED");
         $monitor("%b%b%b%b   %b %b %b %b %b %b %b %b %b",numero[4],numero[3], numero[2], numero[1], A,B,C,D,E,F,G,LED,reset);
 
-        reset = 1'b1;
-        clk = 1; insere = 1;
+        reset = 1'b0;
+        clk = 1; insere = 0;
         numero = 4'b0101;
-        reset = 0;
+        reset = 1;
 
-        #30
+        #15
+        insere =1;
+        #10
+        insere =0;
         numero = 4'b1001;
 
-        #20
+
+        #10
+        insere =1;
+
+        #10
         numero = 4'b0000;
+        insere =0;
 
-        #30
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1001;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1000;
+        insere =0;
+
+        #10
+        insere =1;
+        #10
+        numero = 4'b0001;
+        insere =0;
+
+        #10
+        insere =1;
+        #15
+        reset= 0;
+        #15
+//-------------------------------------------------------------------------------------
+        reset = 1'b1;
+        clk = 1; insere = 0;
+        numero = 4'b0101;
+
+        #15
+        insere =1;
+        #10
+        insere =0;
         numero = 4'b1001;
 
-        #30
-        numero = 4'b1000;
+        #10
+        insere =1;
 
-        #30
+        #10
+        numero = 4'b0000;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
         numero = 4'b0001;
+        insere =0;
 
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1001;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1000;
+        insere =0;
+
+        #10
+        insere =1;
+        #10
+        numero = 4'b0001;
+        insere =0;
+        #10
+        insere =1;
+        #15
+        reset= 0;
+        #15
 //-------------------------------------------------------------------------------------
-        #10 $finish;
+
+        reset = 1'b1;
+        clk = 1; insere = 0;
+        numero = 4'b0101;
+
+        #15
+        insere =1;
+        #10
+        insere =0;
+        numero = 4'b1001;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b0000;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b0001;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b0010;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1000;
+        insere =0;
+
+        #10
+        insere =1;
+        #10
+        numero = 4'b0001;
+        insere =0;
+        #10
+        insere =1;
+        #15
+        #40 $finish;
     end
 endmodule
