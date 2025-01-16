e4f3a602cd751ab28628baa07884efa8c776ba33
Júlio César <julioitabira@hotmail.com>
2025-01-16 15:03:20 -0300

    Update maquina_tb.v

--- maquina_tb.v
+++ maquina_tb.v
@@ -9,7 +9,7 @@ module maquina_tb ();
 
     always begin
         #5
-        clk <= ~clk;
+        clk = ~clk;
     end
     initial begin
         $dumpfile("maquina.vcd");
@@ -17,73 +17,147 @@ module maquina_tb ();
         $display("numero A B C D E F G LED");
         $monitor("%b%b%b%b   %b %b %b %b %b %b %b %b %b",numero[4],numero[3], numero[2], numero[1], A,B,C,D,E,F,G,LED,reset);
 
-        reset = 1'b1;
-        clk = 1;
+        reset = 1'b0;
+        clk = 1; insere = 0;
         numero = 4'b0101;
-        #10
-        insere = 1;
-        reset = 0;
+        reset = 1;
 
-        #10
-        insere = 0;
-        #20
-        numero = 4'b1001;
-        insere = 1;
-
+        #15
+        insere =1;
         #10
         insere =0;
+        numero = 4'b1001;
+
+
+        #10
+        insere =1;
+
         #10
         numero = 4'b0000;
-        insere = 1;
-        #10
-        insere = 0;
-
-        #10
-        numero = 4'b0010;
-        insere = 1;
-
-        #10
         insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1001;
+        insere =0;
+
+        #10
+        insere =1;
 
         #10
         numero = 4'b1000;
+        insere =0;
+
+        #10
+        insere =1;
+        #10
+        numero = 4'b0001;
+        insere =0;
+
+        #10
+        insere =1;
+        #15
+        reset= 0;
+        #15
+//-------------------------------------------------------------------------------------
+        reset = 1'b1;
+        clk = 1; insere = 0;
+        numero = 4'b0101;
+
+        #15
+        insere =1;
+        #10
+        insere =0;
+        numero = 4'b1001;
+
+        #10
         insere =1;
 
         #10
+        numero = 4'b0000;
         insere =0;
 
+        #10
+        insere =1;
 
         #10
         numero = 4'b0001;
+        insere =0;
+
+        #10
         insere =1;
 
+        #10
+        numero = 4'b1001;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1000;
+        insere =0;
+
+        #10
+        insere =1;
+        #10
+        numero = 4'b0001;
+        insere =0;
+        #10
+        insere =1;
+        #15
+        reset= 0;
+        #15
 //-------------------------------------------------------------------------------------
 
-        #10
-        insere =0;
         reset = 1'b1;
-        #20
-        reset = 0;
+        clk = 1; insere = 0;
         numero = 4'b0101;
 
-
-        #20
+        #15
+        insere =1;
+        #10
+        insere =0;
         numero = 4'b1001;
 
-        #20
-        numero = 4'b0100;
+        #10
+        insere =1;
 
         #10
         numero = 4'b0000;
+        insere =0;
 
-        #20
-        numero = 4'b0010;
+        #10
+        insere =1;
 
-        #20
-        numero = 4'b1000;
-
-        #20
+        #10
         numero = 4'b0001;
-        #10 $finish;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b0010;
+        insere =0;
+
+        #10
+        insere =1;
+
+        #10
+        numero = 4'b1000;
+        insere =0;
+
+        #10
+        insere =1;
+        #10
+        numero = 4'b0001;
+        insere =0;
+        #10
+        insere =1;
+        #15
+        #40 $finish;
     end
 endmodule
